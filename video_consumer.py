7c66f01c96e61b874eed14fbc536110e5c3d4804
Manoj Phatak <manoj.a.phatak@gmail.com>
2020-07-24 13:29:10 +0530

    Code Rafactor: Replace map-filters with 'pipe's

--- video_consumer.py
+++ video_consumer.py
@@ -2,6 +2,7 @@ import os
 import fnmatch
 
 import face_recognition
+from pipe import Pipe, select, where
 
 from kafka_client import KafkaImageCli
 from config import bootstrap_servers, topic
@@ -9,21 +10,25 @@ from config import bootstrap_servers, topic
 basepath= "/home/manoj/Pictures"
 known_faces_path = os.path.join(basepath, "known_faces")
 
+@Pipe
+def tolist(iterable):
+    return list(iterable)
+
 
 def load_known_faces(known_faces_path):
     assert os.path.exists(known_faces_path)
-    imgs = fnmatch.filter(os.listdir(known_faces_path), "*.jpg")
-    names = [os.path.splitext(f)[0] for f in imgs]
-    imgs = [os.path.join(known_faces_path, img) for img in imgs]
+    jpgfiles = fnmatch.filter(os.listdir(known_faces_path), "*.jpg")
+    image_titles = [os.path.splitext(f)[0] for f in jpgfiles]   # Treat filename-without-extn as image title
+    jpgfpaths = [os.path.join(known_faces_path, f) for f in jpgfiles]
 
     known_faces = {}
-    for n,img in zip(names, imgs):
-        image = face_recognition.load_image_file(img)
+    for title,fpath in zip(image_titles, jpgfpaths):
+        image = face_recognition.load_image_file(fpath)
         face_encoding = face_recognition.face_encodings(image)[0]
         hashencod = face_encoding.data.tobytes()
         known_faces[hashencod] = {
-            "name": n,
-            "imgfile": img,
+            "name": title,
+            "imgfile": fpath,
             "face_encoding":face_encoding
         }
     return known_faces
@@ -31,18 +36,18 @@ def load_known_faces(known_faces_path):
 
 def match_faces(faceencod, known_faces, tolerance):
     knonwn_encodes_hashes = known_faces.values()
-    knonwn_encodes = list(map(lambda e: e["face_encoding"], knonwn_encodes_hashes))
-    match = face_recognition.compare_faces(knonwn_encodes, faceencod, tolerance)
     
-    matches = zip(match, knonwn_encodes_hashes)
-    matches = list(filter(lambda x: x[0], matches))
-    matches = list(map(lambda m: m[1], matches))
-    return matches
+    knonwn_encodes = knonwn_encodes_hashes | select(lambda e: e["face_encoding"]) | tolist
+    matches = face_recognition.compare_faces(knonwn_encodes, faceencod, tolerance)
+    
+    return zip(matches, knonwn_encodes_hashes) \
+                                             | where(lambda x: x[0])    \
+                                             | select(lambda m: m[1])     
+
 
 
 def consume_images_from_kafka(kafkaCli):
     known_faces = load_known_faces(known_faces_path)
-    #print(known_faces)
     for m in kafkaCli.consumer:
         print(f"received message from Kafka")
         tempfile = "tempfile.jpg"
