b6256cb431f6476bec0f6f85fd703549fae46b70
Manoj Phatak <manoj.a.phatak@gmail.com>
2020-07-24 11:06:47 +0530

    extract face-encoding & match with known faces with their encodings

--- video_consumer.py
+++ video_consumer.py
@@ -1,20 +1,59 @@
 import os
+import fnmatch
+
+import face_recognition
 
 from kafka_client import KafkaImageCli
 from config import bootstrap_servers, topic
 
-def consume_messages(kafkaCli):
-    frameid = 0
-    basepath= "/home/manoj/Pictures"
+basepath= "/home/manoj/Pictures"
+known_faces_path = os.path.join(basepath, "known_faces")
+
+
+def load_known_faces(known_faces_path):
+    assert os.path.exists(known_faces_path)
+    imgs = fnmatch.filter(os.listdir(known_faces_path), "*.jpg")
+    names = [os.path.splitext(f)[0] for f in imgs]
+    imgs = [os.path.join(known_faces_path, img) for img in imgs]
+
+    known_faces = []
+    for n,img in zip(names, imgs):
+        image = face_recognition.load_image_file(img)
+        face_encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append({
+            "name": n,
+            "imgfile": img,
+            "face_encoding":face_encoding
+        }
+        )
+    return known_faces
+
+
+def match_faces(face_encoding, known_faces, tolerance):
+    matched_faces = []
+    for kf in known_faces:
+        match = face_recognition.compare_faces([kf["face_encoding"]], face_encoding, tolerance)
+        if match:
+            matched_faces.append(kf["name"])
+    return matched_faces
+
+
+def consume_images_from_kafka(kafkaCli):
+    known_faces = load_known_faces(known_faces_path)
+    #print(known_faces)
     for m in kafkaCli.consumer:
-        print("received message from Kafka")
-        fname= f"{frameid}.jpg"           
-        with open(os.path.join(basepath,fname), "wb") as f:
+        print(f"received message from Kafka")
+        tempfile = "tempfile.jpg"
+        with open(os.path.join(basepath,tempfile), "wb") as f:
             f.write(m.value) 
-            frameid += 1
+        image = face_recognition.load_image_file(os.path.join(basepath,tempfile))
+        face_encoding = face_recognition.face_encodings(image)[0]
+        
+        matched_faces = match_faces(face_encoding, known_faces, tolerance=0.9)
+        print(matched_faces)
 
 
 if __name__ == "__main__":
     kafkaCli = KafkaImageCli(bootstrap_servers= bootstrap_servers, topic= topic)
     kafkaCli.register_consumer()
-    consume_messages(kafkaCli)
+    consume_images_from_kafka(kafkaCli)
