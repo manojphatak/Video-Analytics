230a87aca55f2ca73857796287374cb2c779d663
Manoj Phatak <manoj.phatak@hcl.com>
2020-08-12 16:16:52 +0530

    introduced new service: face_matcher

--- generator/restart-containers.sh
+++ generator/restart-containers.sh
@@ -4,4 +4,5 @@ docker-compose stop
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' movie-feeder)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-detector)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' filesystem-consumer)
+truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-matcher)
 docker-compose up -d
