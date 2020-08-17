276a9e7e178845b7cc9179e51d163d7d91da8370
Manoj Phatak <manoj.phatak@hcl.com>
2020-08-17 11:16:58 +0530

    new service added: motion-detector

--- generator/restart-containers.sh
+++ generator/restart-containers.sh
@@ -5,4 +5,5 @@ truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' movie-feeder)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-detector)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' filesystem-consumer)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-matcher)
+truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' motion-detector)
 docker-compose up -d
