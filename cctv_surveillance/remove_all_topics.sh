9c697df30cad51b442358e5f0505be819e67d8da
Manoj Phatak <manoj.phatak@hcl.com>
2020-09-02 20:02:45 +0530

    kafka topics renamed

--- cctv_surveillance/remove_all_topics.sh
+++ cctv_surveillance/remove_all_topics.sh
@@ -5,10 +5,10 @@
 echo "listing topics before deletion:"
 $KAFKA_HOME/bin/kafka-topics.sh --list  --bootstrap-server kafka:9092
 
-$KAFKA_HOME/bin/kafka-topics.sh --delete --topic queueing.raw-frames  --bootstrap-server kafka:9092
-$KAFKA_HOME/bin/kafka-topics.sh --delete --topic transactions.faces  --bootstrap-server kafka:9092
+$KAFKA_HOME/bin/kafka-topics.sh --delete --topic cctv.raw-frames  --bootstrap-server kafka:9092
+$KAFKA_HOME/bin/kafka-topics.sh --delete --topic cctv.faces-detected  --bootstrap-server kafka:9092
 $KAFKA_HOME/bin/kafka-topics.sh --delete --topic transactions.matched-faces  --bootstrap-server kafka:9092
-$KAFKA_HOME/bin/kafka-topics.sh --delete --topic transactions.motion  --bootstrap-server kafka:9092
+$KAFKA_HOME/bin/kafka-topics.sh --delete --topic cctv.motion-detected  --bootstrap-server kafka:9092
 
 echo "listing topics after deletion:"
 $KAFKA_HOME/bin/kafka-topics.sh --list  --bootstrap-server kafka:9092
