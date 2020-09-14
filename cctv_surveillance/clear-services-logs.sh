d41b628afbaad33ad593760533613afa859892df
Manoj Phatak <manoj.phatak@hcl.com>
2020-09-14 11:52:44 +0530

    script for automated tests

--- cctv_surveillance/clear-services-logs.sh
+++ cctv_surveillance/clear-services-logs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-docker-compose stop
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' movie-feeder)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-detector)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' filesystem-consumer)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' face-matcher)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' motion-detector)
 truncate -s 0 $(sudo docker inspect --format='{{.LogPath}}' object-detector)
-docker-compose up -d
